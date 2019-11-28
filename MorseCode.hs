80d394f5dcca4af52de5dcff1825ffe10ef77626
William Bergqvist <william.bergqvist@nicknamed.se>
2019-11-28 17:14:38 +0100

    refactor morse cpde

--- MorseCode.hs
+++ MorseCode.hs
@@ -1,7 +1,6 @@
 module MorseCode where
 
 import Data.Map.Strict (fromList, (!))
-import Data.List.Split
 
 morseCodes = fromList([
     ("....","H")
@@ -14,20 +13,18 @@ morseCodes = fromList([
 removeExcessWhitespace :: String -> Bool -> String
 removeExcessWhitespace [] _ = []
 removeExcessWhitespace [fst] _ = if fst == ' ' then "" else [fst]
-removeExcessWhitespace (fst:snd:rest) isLongSpace = 
-    if fst == ' ' && snd == ' ' && isLongSpace then
-        removeExcessWhitespace rest isLongSpace
-    else if fst == ' ' && snd == ' '
-        then  ' ' : '|' : removeExcessWhitespace rest True
-    else fst : removeExcessWhitespace (snd : rest) False
+removeExcessWhitespace (fst:snd:rest) isLongSpace
+    | fst == ' ' && snd == ' ' && isLongSpace = removeExcessWhitespace rest isLongSpace
+    | fst == ' ' && snd == ' ' = ' ' : '|' : removeExcessWhitespace rest True
+    | otherwise = fst : removeExcessWhitespace (snd:rest) False
 
 
 decodeMorse :: String -> String
 decodeMorse str = 
     let
         morseWithoutWhitespace = removeExcessWhitespace str False
-        codes = splitOn " " morseWithoutWhitespace
+        codes = words morseWithoutWhitespace
     in
-        foldl (\acc code -> if code == " " then acc else acc ++ (morseCodes ! code)) "" codes
+        foldl (\acc code -> if code == "|" then acc ++ " " else acc ++ (morseCodes ! code)) "" codes
 
 --morseCodes ! ".--"
